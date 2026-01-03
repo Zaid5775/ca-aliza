4fd78507f6facea28d6c73e26dc125782aeefb75
zaid5775 <szaid5775@gmail.com>
2026-01-03 16:58:42 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ export default function App() {
         <Stickers />
         <SurpriseCards />
         <ExamMotivation />
-        <Timeline />
         <CuteLetter />
+        <Timeline />
         <SendAHug />
        
 
